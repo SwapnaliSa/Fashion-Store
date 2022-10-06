c9d3e0f2157706926702b9d468f4fe36f30e42e2
Rahul Sharma <rahulat4200@gmail.com>
2022-10-06 11:20:59 +0530

    Step 25 completed. Single product details screen Reducer and Action done.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,21 +1,24 @@
 import { Button, Flex, Grid, Heading, Image, Text } from '@chakra-ui/react';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link as RouterLink, useParams } from 'react-router-dom';
 
+import { listProductDetails } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
 import Rating from '../components/Rating';
 
 const ProductScreen = () => {
+	const dispatch = useDispatch();
+
 	const { id } = useParams();
-	const [product, setProduct] = useState({});
+
+	const productDetails = useSelector((state) => state.productDetails);
+	const { loading, product, error } = productDetails;
 
 	useEffect(() => {
-		const fetchProduct = async () => {
-			const { data } = await axios.get(`/api/products/${id}`);
-			setProduct(data);
-		};
-		fetchProduct();
-	}, [id]);
+		dispatch(listProductDetails(id));
+	}, [id, dispatch]);
 
 	return (
 		<>
@@ -25,62 +28,68 @@ const ProductScreen = () => {
 				</Button>
 			</Flex>
 
-			<Grid templateColumns='5fr 4fr 3fr' gap='10'>
-				{/* Column 1 */}
-				<Image src={product.image} alt={product.name} borderRadius='md' />
+			{loading ? (
+				<Loader />
+			) : error ? (
+				<Message type='error'>{error}</Message>
+			) : (
+				<Grid templateColumns='5fr 4fr 3fr' gap='10'>
+					{/* Column 1 */}
+					<Image src={product.image} alt={product.name} borderRadius='md' />
 
-				{/* Column 2 */}
-				<Flex direction='column'>
-					<Heading as='h6' fontSize='base' color='gray.500'>
-						{product.brand}
-					</Heading>
+					{/* Column 2 */}
+					<Flex direction='column'>
+						<Heading as='h6' fontSize='base' color='gray.500'>
+							{product.brand}
+						</Heading>
 
-					<Heading as='h2' fontSize='4xl' mb='4'>
-						{product.name}
-					</Heading>
+						<Heading as='h2' fontSize='4xl' mb='4'>
+							{product.name}
+						</Heading>
 
-					<Rating
-						value={product.rating}
-						text={`${product.numReviews} reviews`}
-					/>
+						<Rating
+							value={product.rating}
+							text={`${product.numReviews} reviews`}
+						/>
 
-					<Heading
-						as='h5'
-						my='5'
-						fontSize='4xl'
-						fontWeight='bold'
-						color='teal.600'>
-						₹{product.price}
-					</Heading>
+						<Heading
+							as='h5'
+							my='5'
+							fontSize='4xl'
+							fontWeight='bold'
+							color='teal.600'>
+							₹{product.price}
+						</Heading>
 
-					<Text>{product.description}</Text>
-				</Flex>
-
-				{/* Column 3 */}
-				<Flex direction='column'>
-					<Flex justifyContent='space-between' py='2'>
-						<Text>Price: </Text>
-						<Text fontWeight='bold'>₹{product.price}</Text>
+						<Text>{product.description}</Text>
 					</Flex>
 
-					<Flex justifyContent='space-between' py='2'>
-						<Text>Status: </Text>
-						<Text fontWeight='bold'>
-							{product.countInStock > 0 ? 'In Stock' : 'Not Available'}
-						</Text>
-					</Flex>
+					{/* Column 3 */}
+					<Flex direction='column'>
+						<Flex justifyContent='space-between' py='2'>
+							<Text>Price: </Text>
+							<Text fontWeight='bold'>₹{product.price}</Text>
+						</Flex>
 
-					<Button
-						bg='gray.800'
-						colorScheme='teal'
-						my='2'
-						textTransform='uppercase'
-						letterSpacing='wide'
-						disabled={product.countInStock === 0}>
-						Add to cart
-					</Button>
-				</Flex>
-			</Grid>
+						<Flex justifyContent='space-between' py='2'>
+							<Text>Status: </Text>
+							<Text fontWeight='bold'>
+								{product.countInStock > 0 ? 'In Stock' : 'Not Available'}
+							</Text>
+						</Flex>
+
+						<Button
+							bg='gray.800'
+							colorScheme='teal'
+							my='2'
+							textTransform='uppercase'
+							letterSpacing='wide'
+							disabled={product.countInStock === 0}>
+							Add to cart
+						</Button>
+					</Flex>
+				</Grid>
+			)}
 		</>
 	);
 };
