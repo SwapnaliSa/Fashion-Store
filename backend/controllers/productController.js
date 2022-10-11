f37db1bbcd4264779691addba3614f7d6d4abefa
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2022-10-11 11:29:54 +0530

    Step 33 completed. Creating custom Authentication Middleware to access protected routes done.

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,11 +1,21 @@
 import asyncHandler from 'express-async-handler';
 import Product from '../models/productModel.js';
 
+/**
+ * @desc		Fetch all products
+ * @route		GET /api/products
+ * @access	public
+ */
 const getProducts = asyncHandler(async (req, res) => {
 	const products = await Product.find({});
 	res.json(products);
 });
 
+/**
+ * @desc		Fetch single product
+ * @route		GET /api/products/:id
+ * @access	public
+ */
 const getProductById = asyncHandler(async (req, res) => {
 	try {
 		const product = await Product.findById(req.params.id);
