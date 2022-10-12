1b75b4bec14517133bd5a0a798de54983d8f4eae
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2022-10-12 12:01:02 +0530

    Step 41 completed. User Profile Screen and Getting User Details done.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
@@ -28,6 +29,7 @@ const App = () => {
 					<Route path='/cart' element={<CartScreen />} />
 					<Route path='/login' element={<LoginScreen />} />
 					<Route path='/register' element={<RegisterScreen />} />
+					<Route path='/profile' element={<ProfileScreen />} />
 				</Routes>
 			</Flex>
 			<Footer />
