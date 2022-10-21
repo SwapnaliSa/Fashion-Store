d23a1fe518adad3d1826103d8b0ce9c2443fa8b2
Rahul Sharma <54216803+rahul4200@users.noreply.github.com>
2022-10-21 12:09:32 +0530

    Step 68 and 69 completed. Admin - Update Product Functionality done.

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -12,6 +12,10 @@ import {
 	PRODUCT_LIST_FAIL,
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
+	PRODUCT_UPDATE_FAIL,
+	PRODUCT_UPDATE_REQUEST,
+	PRODUCT_UPDATE_RESET,
+	PRODUCT_UPDATE_SUCCESS,
 } from '../constants/productConstants';
 
 export const listProducts = () => async (dispatch) => {
@@ -99,6 +103,39 @@ export const createProduct = () => async (dispatch, getState) => {
 	} catch (err) {
 		dispatch({
 			type: PRODUCT_CREATE_FAIL,
+			payload:
+				err.response && err.response.data.message
+					? err.response.data.message
+					: err.message,
+		});
+	}
+};
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+	try {
+		dispatch({ type: PRODUCT_DELETE_REQUEST });
+
+		const {
+			userLogin: { userInfo },
+		} = getState();
+
+		const config = {
+			headers: {
+				Authorization: `Bearer ${userInfo.token}`,
+				'Content-Type': 'application/json',
+			},
+		};
+
+		const { data } = await axios.put(
+			`/api/products/${product._id}`,
+			product,
+			config
+		);
+
+		dispatch({ type: PRODUCT_UPDATE_SUCCESS, payload: data });
+	} catch (err) {
+		dispatch({
+			type: PRODUCT_DELETE_FAIL,
 			payload:
 				err.response && err.response.data.message
 					? err.response.data.message
